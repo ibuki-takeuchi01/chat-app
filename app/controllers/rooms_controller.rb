e766ed8df429157465b1f54e36b9568ce8180507
ibuki-takeuchi01 <tomica.0324@gmail.com>
2021-06-02 12:01:30 +0900

    メッセージ送信機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,7 +2,7 @@ class RoomsController < ApplicationController
 
   def index
   end
-  
+
   def new
     @room = Room.new
   end
@@ -16,4 +16,9 @@ class RoomsController < ApplicationController
     end
   end
 
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
+  end
 end
